9b4a976495dd44ba99dabdb497e5ebcfd5d25a75
anscom <anscom.ooi@silverlakeaxis.com>
2025-08-06 15:47:41 +0800

    First frontend commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",     // if you're using Vite or plain HTML
+    "./src/**/*.{js,jsx,ts,tsx}" // for React files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
